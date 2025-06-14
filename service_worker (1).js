2531c7295e1e4be46c93042d5ab7689b6ea63ef6
PYLD89 <100071613+PYLD89@users.noreply.github.com>
2025-06-14 19:04:29 +0000

    Add files via upload

---
+++ service_worker (1).js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'medapp-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
